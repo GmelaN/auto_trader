7fdd3382993717aa6ef4cad47cc58e42354a0a80
GmelaN <gmelanals@gmail.com>
2022-04-17 16:40:50 +0900

    add fetvh data by fdr

--- get_data.py
+++ get_data.py
@@ -1,3 +1,20 @@
 import numpy as np
 import pandas as pd
-from sklearn.impute import SimpleImputer
+import FinanceDataReader as fdr
+
+# fetch BNK by day
+data = fdr.DataReader("138930", "2020-01-01")
+with open("BNK_by_day.csv", 'w', encoding="UTF8") as f:
+    data.to_csv(f)
+
+# fetch S&P500 by day
+data = fdr.DataReader("US500", "2020-01-01")
+with open("SP500_by_day.csv", 'w', encoding="UTF8") as f:
+    data.to_csv(f)
+
+# fetvh USD-KRW by day
+data = fdr.DataReader("USD/KRW", "2020-01-01")
+with open("USD-KRW_by_day.csv", 'w', encoding="UTF8") as f:
+    data.to_csv(f)
+
+
